62acf8817c88d361bb2f8b6d8c440df19ca9d386
yuriy <dyachkovy00@gmail.com>
2025-02-18 19:44:14 +0300

    successfully exported some test data to google sheets

--- back/routes/web.php
+++ back/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\GoogleSheetsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/export-to-sheets', [GoogleSheetsController::class, 'export']);
